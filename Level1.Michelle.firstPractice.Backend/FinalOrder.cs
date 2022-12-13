309943b9c3938c5030f805aafb8e98d995268877
Michelle <michelle.cadavid@jala.university>
2022-12-13 18:07:38 -0500

    changes

--- Level1.Michelle.firstPractice.Backend/FinalOrder.cs
+++ Level1.Michelle.firstPractice.Backend/FinalOrder.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Level1.Michelle.firstPractice;
 
 namespace Level1.Michelle.firstPractice.Backend
 {
     internal class FinalOrder
     {
+        public string ClientName { get; set; }
+        public int OrderNumber { get; set; }
+
+
+
+        public FinalOrder(string cName, int orderNumber ) 
+        { 
+        this.ClientName = cName;
+        this.OrderNumber = orderNumber;
+        List<object> OrderList = new();
+        }
+     
+        public void CalculateTotal()
+        {
+
+        }
 
     }
 }
